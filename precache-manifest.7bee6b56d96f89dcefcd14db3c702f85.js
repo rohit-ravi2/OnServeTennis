4123efd0990250cd79b4d1a9bb450eec4ccfbdd3
Rohit Ravi <rohit.ravi2@gmail.com>
2022-06-22 16:03:49 -0400

    Updates

---
+++ precache-manifest.7bee6b56d96f89dcefcd14db3c702f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0ddaba093029ec817a0b74ffbcaa54",
+    "url": "/OnServeTennis/index.html"
+  },
+  {
+    "revision": "a5d8544c81b9f77cf812",
+    "url": "/OnServeTennis/static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "db1eb2e821edb692385e",
+    "url": "/OnServeTennis/static/css/main.a6d3ab00.chunk.css"
+  },
+  {
+    "revision": "a5d8544c81b9f77cf812",
+    "url": "/OnServeTennis/static/js/2.117d4415.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/OnServeTennis/static/js/2.117d4415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1eb2e821edb692385e",
+    "url": "/OnServeTennis/static/js/main.6d49920b.chunk.js"
+  },
+  {
+    "revision": "117b660197c5f59d5f2d",
+    "url": "/OnServeTennis/static/js/runtime-main.e2af17e0.js"
+  }
+]);
